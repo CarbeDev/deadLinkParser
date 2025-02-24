f90bfee57ba9ae78324bcc6668621181e86e2c76
Kevin Carbeti <kevincarbeti@gmail.com>
2025-02-24 15:41:54 +0100

    call internal page

---
+++ data/appData.go
@@ -0,0 +1,49 @@
+package data
+
+type AppData struct {
+	InitialUrl string
+	FoundLinks []FoundLink
+}
+
+type FoundLink struct {
+	Link    string
+	Visited bool
+	Alive   bool
+}
+
+func InitialiseAppData(baseUrl string) AppData {
+	//FIXME add link verification
+	return AppData{
+		InitialUrl: baseUrl,
+		FoundLinks: []FoundLink{},
+	}
+}
+
+func (data AppData) hasLink(link string) bool {
+	for _, foundLink := range data.FoundLinks {
+		if link == foundLink.Link {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (data AppData) HasUncheckedLink() bool {
+	for _, link := range data.FoundLinks {
+		if !link.Visited {
+			return true
+		}
+	}
+
+	return false
+}
+
+func AddLinkFound(link string, appData *AppData) {
+	if !appData.hasLink(link) {
+		*appData = AppData{
+			InitialUrl: appData.InitialUrl,
+			FoundLinks: append(appData.FoundLinks, FoundLink{Link: link}),
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,34 +1,73 @@
 package main
 
 import (
-	"fmt"
+	"deadLinkParser/data"
 	"golang.org/x/net/html"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	url := "https://scrape-me.dreamsofcode.io/"
 
+	appData := data.InitialiseAppData(url)
+
+	links := getHrefLinks(url)
+
+	for _, link := range links {
+		data.AddLinkFound(link, &appData)
+	}
+
+	index := 0
+
+	for index < len(appData.FoundLinks) {
+		if !appData.HasUncheckedLink() {
+			break
+		}
+
+		selectedLink := appData.FoundLinks[index].Link
+		if strings.HasPrefix(selectedLink, "/") {
+			fullUrl := buildUrl(selectedLink, url)
+
+			newLinks := getHrefLinks(fullUrl)
+
+			for _, link := range newLinks {
+				data.AddLinkFound(link, &appData)
+			}
+		}
+
+		index++
+
+		log.Print(appData)
+	}
+
+}
+
+func getHrefLinks(url string) []string {
 	resp, err := http.Get(url)
 
 	if err != nil {
 		log.Fatalf("Error requesting url '%v' : %v", url, err)
 	}
 
-	//readable, _ := io.ReadAll(resp.Body)
-	//log.Print(string(readable))
-
 	content, err := html.Parse(resp.Body)
+
+	var links []string
 
 	for element := range content.Descendants() {
 		if element.Type == html.ElementNode && element.Data == "a" {
 			for _, attribute := range element.Attr {
 				if attribute.Key == "href" {
-					fmt.Println(attribute.Val)
+					links = append(links, attribute.Val)
 				}
 			}
 		}
 	}
 
+	return links
+}
+
+func buildUrl(link string, baseUrl string) string {
+	return baseUrl + link[1:]
 }
